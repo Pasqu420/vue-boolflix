949ba5f67926f1ac5af04a021e07be16996f7da8
pasqu <pasquale.bucciero97@gmailcom>
2021-04-19 17:30:44 +0200

    ricerca film e serie tv; aggiunto bandiera a item.original_language;

--- script.js
+++ script.js
@@ -8,17 +8,34 @@ function boolFlix() {
     methods:{
       search: function () {
         axios.get('https://api.themoviedb.org/3/search/multi',{
-          params:{
-            'api_key':'751a05be1460b8ba83b49cc31a439091',
-            'query': this.input
-          }
-        })
+            params:{
+              'api_key':'751a05be1460b8ba83b49cc31a439091',
+              'query': this.input,
+              'include_image_language':'en,null'
+            }
+          })
         .then(data => {
           const result = data.data.results;
           // console.log(result);
           this.contents = result
           console.log(this.contents);
         });
+      },
+      language: function (item) {
+        if (item == 'en') {
+          return 'img/Bandiera-Inglese.png';
+        }else if (item == 'it') {
+          return 'img/Bandiera-italiana.jpg';
+        }else if (item == 'es') {
+          return 'img/bandiera-Spain.svg';
+        }
+      },
+      overview: function (item){
+        if (item.overview && item.overview.length > 130) {
+          return item.overview.slice(0,130);
+        }else {
+          return item.overview;
+        }
       }
     }
   });
